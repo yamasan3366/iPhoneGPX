6031b7eb77cc013cd1f026da32ed03b9b89b148a
yamasan3366 <y_yamakoshi@yahoo.co.jp>
2026-03-06 09:56:27 +0900

    361

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: V7TqXT9J */
+/* Manifest version: qHt7FB6F */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
